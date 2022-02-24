3c985d7eac2dbfbfd98163a7ae9596e1dce9de1c
eriqjaffe <eriqjaffe@gmail.com>
2022-02-24 10:50:24 -0600

    initial commmit

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, dialog } = require('electron')
+const { app, BrowserWindow, dialog, Menu, shell } = require('electron')
 const path = require('path')
 const fs = require('fs')
 const os = require('os')
@@ -6,10 +6,102 @@ const express = require('express')
 const Jimp = require('jimp')
 const openExplorer = require('open-file-explorer');
 const imagemagickCli = require('imagemagick-cli');
+const ttfInfo = require('ttfinfo')
+const url = require('url');
+const output = require('image-output')
 
 const app2 = express()
 const port = 8086;
 const tempDir = os.tmpdir()
+const isMac = process.platform === 'darwin'
+
+const template = [
+// { role: 'appMenu' }
+...(isMac ? [{
+	label: app.name,
+	submenu: [
+	{ role: 'about' },
+	{ type: 'separator' },
+	{ role: 'services' },
+	{ type: 'separator' },
+	{ role: 'hide' },
+	{ role: 'hideOthers' },
+	{ role: 'unhide' },
+	{ type: 'separator' },
+	{ role: 'quit' }
+	]
+}] : []),
+// { role: 'fileMenu' }
+{
+	label: 'File',
+	submenu: [
+	isMac ? { role: 'close' } : { role: 'quit' }
+	]
+},
+// { role: 'viewMenu' }
+{
+	label: 'View',
+	submenu: [
+	{ role: 'reload' },
+	{ role: 'forceReload' },
+	{ role: 'toggleDevTools' },
+	{ type: 'separator' },
+	{ role: 'resetZoom' },
+	{ role: 'zoomIn' },
+	{ role: 'zoomOut' },
+	{ type: 'separator' },
+	{ role: 'togglefullscreen' }
+	]
+},
+// { role: 'windowMenu' }
+{
+	label: 'Window',
+	submenu: [
+	{ role: 'minimize' },
+	{ role: 'zoom' },
+	...(isMac ? [
+		{ type: 'separator' },
+		{ role: 'front' },
+		{ type: 'separator' },
+		{ role: 'window' }
+	] : [
+		{ role: 'close' }
+	])
+	]
+},
+{
+	role: 'help',
+	submenu: [
+	{
+		label: 'About Franchise Hockey Manager',
+		click: async () => {    
+		await shell.openExternal('https://www.ootpdevelopments.com/franchise-hockey-manager-home/')
+		}
+	},
+	{
+		label: 'About Node.js',
+		click: async () => {    
+		await shell.openExternal('https://nodejs.org/en/about/')
+		}
+	},
+	{
+		label: 'About Electron',
+		click: async () => {
+		await shell.openExternal('https://electronjs.org')
+		}
+	},
+	{
+		label: 'View project on GitHub',
+		click: async () => {
+		await shell.openExternal('https://github.com/eriqjaffe/FHM-Rink-Factory')
+		}
+	}
+	]
+}
+]
+
+const menu = Menu.buildFromTemplate(template)
+Menu.setApplicationMenu(menu)
 
 app2.listen(port, () => {
   console.log(`Server running on port ${port}`);
@@ -25,23 +117,77 @@ app2.get("/uploadImage", (req, res) => {
 		]
 	  }).then(result => {
 		  if(!result.canceled) {
-			Jimp.read(result.filePaths[0], (err, image) => {
-				if (err) {
-					console.log(err);
-				} else {
-					image.getBase64(Jimp.AUTO, (err, ret) => {
-						res.json({
-							"filename": path.basename(result.filePaths[0]),
-							"image": ret
-						  });
-						res.end();
-					})
-				}
-			});
+			var uploadedImg = null;
+			console.log(path.extname(result.filePaths[0]))
+			if (path.extname(result.filePaths[0]).toLowerCase() == ".gif") {
+				imagemagickCli.exec('magick convert '+result.filePaths[0]+' '+tempDir+'/temp.png').then(({ stdout, stderr }) => {
+					Jimp.read(tempDir+'/temp.png', (err, image) => {
+						if (err) {
+							console.log(err);
+						} else {
+							image.getBase64(Jimp.AUTO, (err, ret) => {
+								res.json({
+									"filename": path.basename(result.filePaths[0]),
+									"image": ret
+								  });
+								res.end();
+							})
+						}
+					});
+				})
+			} else {
+				Jimp.read(result.filePaths[0], (err, image) => {
+					if (err) {
+						console.log(err);
+					} else {
+						image.getBase64(Jimp.AUTO, (err, ret) => {
+							res.json({
+								"filename": path.basename(result.filePaths[0]),
+								"image": ret
+							  });
+							res.end();
+						})
+					}
+				});
+			}
+			
 		  }
 	  }).catch(err => {
 		console.log(err)
 	  })
+})
+
+app2.get("/customFont", (req, res) => {
+	dialog.showOpenDialog(null, {
+		properties: ['openFile'],
+		filters: [
+			{ name: 'Fonts', extensions: ['ttf', 'otf'] }
+		]
+	}).then(result => {
+		if(!result.canceled) {
+			ttfInfo(result.filePaths[0], function(err, info) {
+			var ext = getExtension(result.filePaths[0])
+				var fontPath = url.pathToFileURL(tempDir + '/'+path.basename(result.filePaths[0]))
+				fs.copyFile(result.filePaths[0], tempDir + '/'+path.basename(result.filePaths[0]), (err) => {
+					if (err) {
+						console.log(err)
+					} else {
+						res.json({
+							"fontName": info.tables.name[1],
+							"fontStyle": info.tables.name[2],
+							"familyName": info.tables.name[6],
+							"fontFormat": ext,
+							"fontMimetype": 'font/' + ext,
+							"fontData": fontPath.href
+						});
+						res.end()
+					}
+				})
+			});
+		}
+	}).catch(err => {
+		console.log(err)
+	})
 })
 
 app2.post("/removeBorder", (req, res) => {
@@ -212,17 +358,22 @@ function createWindow () {
     const mainWindow = new BrowserWindow({
       width: 1500,
       height: 760,
-      icon: (__dirname + '/images/hockey-rink.png'),
+      icon: (__dirname + '/images/zamboni.png'),
       webPreferences: {
         //preload: path.join(__dirname, 'preload.js')
       }
     })
   
     mainWindow.loadFile('index.html')
+
+	mainWindow.webContents.on('new-window', function(e, url) {
+		e.preventDefault();
+		shell.openExternal(url);
+	});
   
     // Open the DevTools.
-    mainWindow.maximize()
-    mainWindow.webContents.openDevTools()
+    // mainWindow.maximize()
+    // mainWindow.webContents.openDevTools()
   }
   
   app.whenReady().then(() => {
